bc1a311a42e748d8affb932dcb9aa57ad8af7966
eeeboooooy <zheka.mtb@gmail.com>
2020-10-05 23:36:11 +0300

    Updates

---
+++ precache-manifest.c4c3409a5e3a62495dad73b1831421bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1983292c3eac55487f859cab03e6d772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca9c125070a28c4933f",
+    "url": "/static/css/main.6b9bb492.chunk.css"
+  },
+  {
+    "revision": "d90d923fa3da81e4ad05",
+    "url": "/static/js/2.0cf047e2.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.0cf047e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca9c125070a28c4933f",
+    "url": "/static/js/main.5b4d21ca.chunk.js"
+  },
+  {
+    "revision": "eff801c3d4c83c38d953",
+    "url": "/static/js/runtime-main.c9b8c67c.js"
+  }
+]);
